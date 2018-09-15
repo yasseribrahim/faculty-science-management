6a67b17955f492ff07d9625ec95f351ec8574849
yasser <yasser.ibrahim@principal.software>
2018-09-15 14:01:58 +0200

    upload project

---
+++ greatbritain/includes/config.php
@@ -0,0 +1,4 @@
+<?
+$connection=mysql_connect("localhost","root","zxcvbnm098");
+$db=mysql_select_db("erp",$connection);
+?>
---
+++ includes/constants.inc.php
@@ -0,0 +1,7 @@
+<?php
+
+  define('DB_SERVER', 'localhost');
+  define('DB_SERVER_USERNAME', 'root');
+  define('DB_SERVER_PASSWORD', 'zxcvbnm098');
+  define('DB_DATABASE', 'erp');
+?>
